af76a4b8467dcb8811916fcca86df9c7b15e2605
Augusto Cesar de Assis <augusto@movtech.com.br>
2021-11-05 09:27:17 -0300

    Commit inicial

---
+++ api/SalesForceApi/SalesForceApi/Controllers/OrcamentoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesForceApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrcamentoController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<OrcamentoController> _logger;
+
+        public OrcamentoController(ILogger<OrcamentoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Orcamento> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Orcamento
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
